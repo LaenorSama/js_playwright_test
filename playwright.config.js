a1d023050d7391af4ffa637e46af944d55833673
Laenor Sama <55910514+LaenorSama@users.noreply.github.com>
2025-02-11 19:08:59 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   testDir: "./tests",
   reporter: [
     ["list"],
-    ["allure-playwright", { resultsDir: "./out/allure-results" } ],
+    ["allure-playwright", { resultsDir: "./out/allure-results" }, ],
 });
